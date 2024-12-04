10fab4c529c951ee77cbaf5505532ad4e3949e9b
Tiny-Quant <tay.42@gmail.com>
2024-12-04 17:13:34 -0600

    Initial inference script.

---
+++ Program 3/cs5322f24.py
@@ -0,0 +1,12 @@
+
+def WSD_Test_Rubbish(sentences: list[str]) -> list[int]: # 1 or 2
+    pass 
+
+def WSD_Test_Tissue(sentences: list[str]) -> list[int]: # 1 or 2
+    pass 
+
+def WSD_Test_Overtime(sentences: list[str]) -> list[int]: # 1 or 2
+    pass 
+
+
+
